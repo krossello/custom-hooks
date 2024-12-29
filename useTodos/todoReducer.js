5b8f8145f52ab5f07b44db76d962f9c58b7ab2a5
Kari <karina.rossello@gmail.com>
2024-12-29 15:43:08 -0300

    Se actualiza estructura

---
+++ useTodos/todoReducer.js
@@ -0,0 +1,31 @@
+// { type: [todo remove], payload: id }
+
+export const todoReducer = ( initialState = [], action ) => {  // [] por si me llega vacío, que use como valor inicial un array vacío
+
+
+    switch ( action.type ) {
+        case '[TODO] Add Todo':
+            return [ ...initialState, action.payload ];
+
+        case '[TODO] Remove Todo':
+            return initialState.filter( todo => todo.id !== action.payload );
+
+        case '[TODO] Toggle Todo':
+            return initialState.map( todo => {
+
+                if ( todo.id === action.payload ) {// id
+                    return {
+                        ...todo,
+                        done: !todo.done
+                    }
+                } 
+
+                return todo;
+            });
+    
+        default:
+            return initialState;
+    }
+
+
+}
